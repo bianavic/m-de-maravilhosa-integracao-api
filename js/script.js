c54ee1e1ce588e81f74613888ad2124ef18929d8
bianavic <bianavic@gmail.com>
2019-05-22 19:51:01 -0300

    Retirei codigos desnecessarios

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
-const maravilhosas = document.querySelector('.maravilhosas__box');
 
-
-fetch(`https://theblackwomanhistory.firebaseio.com/.json
-`)
+fetch(`https://theblackwomanhistory.firebaseio.com/.json`)
 .then((response) => {
-    return response.json();
+    return response.json()
 })
 .then((data) => {
-    console.log(data.content);
-
-    data.content.forEach(mulheres => {
+    console.log(data);
+    
+    const maravilhosas = document.querySelector('.maravilhosas__box')
+    
+    data.content.forEach(mulher => {
         let box = document.createElement('div');
         box.setAttribute('class', 'maravilhosas__perfil');
-        box.setAttribute('box-id', data.id);
-        maravilhosas.appendChild(box);
+        maravilhosas.appendChild(box)
 
+        let link = document.createElement('a');
+        link.setAttribute('href', '#');
+        box.appendChild(link)
+
+        let img = document.createElement('img');
+        img.setAttribute('class', 'img-responsive');
+        if (mulher.metadata) {
+            if (mulher.metadata.image) {
+                if (mulher.metadata.image.url) {
+                    img.setAttribute('src', mulher.metadata.image.url);
+                }
+            } else {
+                img.setAttribute('src', './img/img-mulher.png');
+            }
+        } else {
+            img.setAttribute('src', './img/img-mulher.png');
+        }
+        link.appendChild(img);
+        
         let name = document.createElement('p');
-        name.innerHTML = mulheres.title
-        box.appendChild(name);
-
-    
-
-});
-
+        name.innerHTML = mulher.title
+        box.appendChild(name)
+        
+    });
 })
-.catch(function(error){
-    console.log('ERRORR')
+.catch(function(erro){
+    console.log('ERRO!')
 })
