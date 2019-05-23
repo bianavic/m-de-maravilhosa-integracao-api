e4209562bea1154841703df9f7170bd0e61de93c
bianavic <bianavic@gmail.com>
2019-05-22 21:20:11 -0300

    exercicio concluido

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ fetch(`https://theblackwomanhistory.firebaseio.com/.json`)
     return response.json()
 })
 .then((data) => {
-    console.log(data);
     
     const maravilhosas = document.querySelector('.maravilhosas__box')
     
-    data.content.forEach(mulher => {
+    data.content.forEach((mulher) => {
+
         let box = document.createElement('div');
         box.setAttribute('class', 'maravilhosas__perfil');
         maravilhosas.appendChild(box)
@@ -19,6 +19,28 @@ fetch(`https://theblackwomanhistory.firebaseio.com/.json`)
 
         let img = document.createElement('img');
         img.setAttribute('class', 'img-responsive');
+
+         /* 
+        QUARTO - TERNARIO
+        mulher.metadata && mulher.metadata.image ? img.setAttribute('src', mulher.metadata.image.url) : img.setAttribute(/img/img-mulher.png)
+        */
+      /*   TERCEIRO
+        if(mulher.metadata == undefined  mulher.metadata.image == "") {
+            img.setAttribute('src', './img/img-mulher.png')
+        }else {
+            img.setAttribute('src', mulher.metadata.image.url)
+        } */
+     
+         
+   /*      SEGUNDO
+        if(mulher.metadata && mulher.metadata.image){
+            img.setAttribute('src', mulher.metadata.image.url)
+        } else {
+            img.setAttribute('src', './img/img-mulher.png')
+        } */
+
+        
+        //  PRIMEIRO
         if (mulher.metadata) {
             if (mulher.metadata.image) {
                 if (mulher.metadata.image.url) {
@@ -30,6 +52,7 @@ fetch(`https://theblackwomanhistory.firebaseio.com/.json`)
         } else {
             img.setAttribute('src', './img/img-mulher.png');
         }
+        
         link.appendChild(img);
         
         let name = document.createElement('p');
